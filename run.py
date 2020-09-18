5835b2c11f32ccbd88efb5e0a8364f66492e93b1
Josh Rickard <josh.rickard@ip-192-168-1-2.us-west-2.compute.internal>
2020-09-18 16:36:49 -0500

    modify README generation image width

--- run.py
+++ run.py
@@ -17,8 +17,7 @@ Just in case you're at home on a video call and you haven't had time to tidy up 
 '''
 for file in onlyfiles:
     title = file.split('.')[0].replace('_',' ').title()
-    readme += '<a href="./images/{}" title="{}"> <img align="center" src="./images/{}" /></a>'.format(file, title, file)
+    readme += '<a href="./images/{}" title="{}"> <img align="center" src="./images/{}" width="540px"/></a>\n'.format(file, title, file)
 
 with open('README.md','w+') as f:
     f.write(readme)
-
